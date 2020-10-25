6db3187d3ff020584f874f912a32c7347d9bb534
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2020-10-25 08:28:51 +0000

    use newest components

--- d.ts/flv.d.ts
+++ d.ts/flv.d.ts
@@ -16,3 +16,76 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// flv.js TypeScript definition file
+
+declare namespace FlvJs {
+    interface MediaSegment {
+        duration: number;
+        filesize?: number;
+        url: string;
+    }
+
+    interface MediaDataSource {
+        type: string;
+        isLive?: boolean;
+        cors?: boolean;
+        withCredentials?: boolean;
+
+        hasAudio?: boolean;
+        hasVideo?: boolean;
+
+        duration?: number;
+        filesize?: number;
+        url?: string;
+
+        segments?: MediaSegment[];
+    }
+
+    interface Config {
+        /**
+         * @desc Enable separated thread for transmuxing (unstable for now)
+         * @defaultvalue false
+         */
+        enableWorker?: boolean;
+        /**
+         * @desc Enable IO stash buffer. Set to false if you need realtime (minimal latency) for live stream
+         *          playback, but may stalled if there's network jittering.
+         * @defaultvalue true
+         */
+        enableStashBuffer?: boolean;
+        /**
+         * @desc Indicates IO stash buffer initial size. Default is `384KB`. Indicate a suitable size can
+         *          improve video load/seek time.
+         */
+        stashInitialSize?: number;
+
+        /**
+         * @desc Same to `isLive` in **MediaDataSource**, ignored if has been set in MediaDataSource structure.
+         * @defaultvalue false
+         */
+        isLive?: boolean;
+
+        /**
+         * @desc Abort the http connection if there's enough data for playback.
+         * @defaultvalue true
+         */
+        lazyLoad?: boolean;
+        /**
+         * @desc Indicates how many seconds of data to be kept for `lazyLoad`.
+         * @defaultvalue 3 * 60
+         */
+        lazyLoadMaxDuration?: number;
+        /**
+         * @desc Indicates the `lazyLoad` recover time boundary in seconds.
+         * @defaultvalue 30
+         */
+        lazyLoadRecoverDuration?: number;
+        /**
+         * @desc Do load after MediaSource `sourceopen` event triggered. On Chrome, tabs which
+         *          be opened in background may not trigger `sourceopen` event until switched to that tab.
+         * @defaultvalue true
+         */
+        deferLoadAfterSourceOpen?: boolean;
+
+        /**
