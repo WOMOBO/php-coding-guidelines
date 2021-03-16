a0297fd6544c3786fa18670650b8f1347a1a8c76
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2021-03-16 13:52:45 +0000

    add css file

--- src/core/mse-controller.js
+++ src/core/mse-controller.js
@@ -65,4 +65,7 @@ class MSEController {
             audio: null
         };
         this._lastInitSegments = {
-            v
+            video: null,
+            audio: null
+        };
+        this._pendingSegments = {
