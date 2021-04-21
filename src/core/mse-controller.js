3a00ea0a69c67ad95f0bc02c3cadfdf9c8af43d7
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2021-04-21 14:07:18 +0000

    Major fixes

--- src/core/mse-controller.js
+++ src/core/mse-controller.js
@@ -187,4 +187,5 @@ class MSEController {
         this._lastInitSegments[is.type] = is;
 
         if (mimeType !== this._mimeTypes[is.type]) {
-            if (!this._mim
+            if (!this._mimeTypes[is.type]) {  // empty, first chance create sourcebuffer
+                firstIni
