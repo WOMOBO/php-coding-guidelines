8b6683b761e3fccf131808a4cd939ed349f264cc
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2021-11-30 12:48:01 +0000

    Fix ref

--- src/core/transmuxing-worker.js
+++ src/core/transmuxing-worker.js
@@ -3,4 +3,6 @@
  *
  * @author zheng qian <xqq@xqq.im>
  *
- * Licensed under the Apache License, Version 2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Y
