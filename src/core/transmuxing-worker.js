d7e0d8020dada92d23a14bb46558ad7839eb7ce2
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2021-12-19 19:20:04 +0000

    [README]

--- src/core/transmuxing-worker.js
+++ src/core/transmuxing-worker.js
@@ -53,4 +53,5 @@ let TransmuxingWorker = function (self) {
                 controller.on(TransmuxingEvents.MEDIA_SEGMENT, onMediaSegment.bind(this));
                 controller.on(TransmuxingEvents.LOADING_COMPLETE, onLoadingComplete.bind(this));
                 controller.on(TransmuxingEvents.RECOVERED_EARLY_EOF, onRecoveredEarlyEof.bind(this));
-                controller.on(TransmuxingEvents.MEDIA_INFO, onMe
+                controller.on(TransmuxingEvents.MEDIA_INFO, onMediaInfo.bind(this));
+                controller.on(TransmuxingEvents
