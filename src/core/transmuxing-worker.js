bb1bfb5d4b8d26e4cfc810de4551e0c5a8a39175
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2021-12-19 22:55:08 +0000

    Delete trailing whitespaces

--- src/core/transmuxing-worker.js
+++ src/core/transmuxing-worker.js
@@ -54,4 +54,5 @@ let TransmuxingWorker = function (self) {
                 controller.on(TransmuxingEvents.LOADING_COMPLETE, onLoadingComplete.bind(this));
                 controller.on(TransmuxingEvents.RECOVERED_EARLY_EOF, onRecoveredEarlyEof.bind(this));
                 controller.on(TransmuxingEvents.MEDIA_INFO, onMediaInfo.bind(this));
-                controller.on(TransmuxingEvents
+                controller.on(TransmuxingEvents.METADATA_ARRIVED, onMetaDataArrived.bind(this));
+                controller.on(T
