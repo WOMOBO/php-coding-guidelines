3413139b934d63e45bc8d20b9d7b915e04bf8ef8
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-01-27 20:06:40 +0000

    The commit message was empty

--- src/demux/flv-demuxer.js
+++ src/demux/flv-demuxer.js
@@ -379,3 +379,84 @@ class FLVDemuxer {
             }
             this._metadata = scriptData;
             let onMetaData = this._metadata.onMetaData;
+
+            if (this._onMetaDataArrived) {
+                this._onMetaDataArrived(Object.assign({}, onMetaData));
+            }
+
+            if (typeof onMetaData.hasAudio === 'boolean') {  // hasAudio
+                if (this._hasAudioFlagOverrided === false) {
+                    this._hasAudio = onMetaData.hasAudio;
+                    this._mediaInfo.hasAudio = this._hasAudio;
+                }
+            }
+            if (typeof onMetaData.hasVideo === 'boolean') {  // hasVideo
+                if (this._hasVideoFlagOverrided === false) {
+                    this._hasVideo = onMetaData.hasVideo;
+                    this._mediaInfo.hasVideo = this._hasVideo;
+                }
+            }
+            if (typeof onMetaData.audiodatarate === 'number') {  // audiodatarate
+                this._mediaInfo.audioDataRate = onMetaData.audiodatarate;
+            }
+            if (typeof onMetaData.videodatarate === 'number') {  // videodatarate
+                this._mediaInfo.videoDataRate = onMetaData.videodatarate;
+            }
+            if (typeof onMetaData.width === 'number') {  // width
+                this._mediaInfo.width = onMetaData.width;
+            }
+            if (typeof onMetaData.height === 'number') {  // height
+                this._mediaInfo.height = onMetaData.height;
+            }
+            if (typeof onMetaData.duration === 'number') {  // duration
+                if (!this._durationOverrided) {
+                    let duration = Math.floor(onMetaData.duration * this._timescale);
+                    this._duration = duration;
+                    this._mediaInfo.duration = duration;
+                }
+            } else {
+                this._mediaInfo.duration = 0;
+            }
+            if (typeof onMetaData.framerate === 'number') {  // framerate
+                let fps_num = Math.floor(onMetaData.framerate * 1000);
+                if (fps_num > 0) {
+                    let fps = fps_num / 1000;
+                    this._referenceFrameRate.fixed = true;
+                    this._referenceFrameRate.fps = fps;
+                    this._referenceFrameRate.fps_num = fps_num;
+                    this._referenceFrameRate.fps_den = 1000;
+                    this._mediaInfo.fps = fps;
+                }
+            }
+            if (typeof onMetaData.keyframes === 'object') {  // keyframes
+                this._mediaInfo.hasKeyframesIndex = true;
+                let keyframes = onMetaData.keyframes;
+                this._mediaInfo.keyframesIndex = this._parseKeyframesIndex(keyframes);
+                onMetaData.keyframes = null;  // keyframes has been extracted, remove it
+            } else {
+                this._mediaInfo.hasKeyframesIndex = false;
+            }
+            this._dispatch = false;
+            this._mediaInfo.metadata = onMetaData;
+            Log.v(this.TAG, 'Parsed onMetaData');
+            if (this._mediaInfo.isComplete()) {
+                this._onMediaInfo(this._mediaInfo);
+            }
+        }
+
+        if (Object.keys(scriptData).length > 0) {
+            if (this._onScriptDataArrived) {
+                this._onScriptDataArrived(Object.assign({}, scriptData));
+            }
+        }
+    }
+
+    _parseKeyframesIndex(keyframes) {
+        let times = [];
+        let filepositions = [];
+
+        // ignore first keyframe which is actually AVC Sequence Header (AVCDecoderConfigurationRecord)
+        for (let i = 1; i < keyframes.times.length; i++) {
+            let time = this._timestampBase + Math.floor(keyframes.times[i] * 1000);
+            times.push(time);
+            filepositions.push(keyframes.filepositions[i]);
