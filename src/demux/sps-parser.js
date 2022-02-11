da5d53854a3912ba1707f2bbb555bcb6e2d2729f
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-02-11 15:26:31 +0000

    Update YT_DeepDark.js

--- src/demux/sps-parser.js
+++ src/demux/sps-parser.js
@@ -15,3 +15,77 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+import ExpGolomb from './exp-golomb.js';
+
+class SPSParser {
+
+    static _ebsp2rbsp(uint8array) {
+        let src = uint8array;
+        let src_length = src.byteLength;
+        let dst = new Uint8Array(src_length);
+        let dst_idx = 0;
+
+        for (let i = 0; i < src_length; i++) {
+            if (i >= 2) {
+                // Unescape: Skip 0x03 after 00 00
+                if (src[i] === 0x03 && src[i - 1] === 0x00 && src[i - 2] === 0x00) {
+                    continue;
+                }
+            }
+            dst[dst_idx] = src[i];
+            dst_idx++;
+        }
+
+        return new Uint8Array(dst.buffer, 0, dst_idx);
+    }
+
+    static parseSPS(uint8array) {
+        let rbsp = SPSParser._ebsp2rbsp(uint8array);
+        let gb = new ExpGolomb(rbsp);
+
+        gb.readByte();
+        let profile_idc = gb.readByte();  // profile_idc
+        gb.readByte();  // constraint_set_flags[5] + reserved_zero[3]
+        let level_idc = gb.readByte();  // level_idc
+        gb.readUEG();  // seq_parameter_set_id
+
+        let profile_string = SPSParser.getProfileString(profile_idc);
+        let level_string = SPSParser.getLevelString(level_idc);
+        let chroma_format_idc = 1;
+        let chroma_format = 420;
+        let chroma_format_table = [0, 420, 422, 444];
+        let bit_depth = 8;
+
+        if (profile_idc === 100 || profile_idc === 110 || profile_idc === 122 ||
+            profile_idc === 244 || profile_idc === 44 || profile_idc === 83 ||
+            profile_idc === 86 || profile_idc === 118 || profile_idc === 128 ||
+            profile_idc === 138 || profile_idc === 144) {
+
+            chroma_format_idc = gb.readUEG();
+            if (chroma_format_idc === 3) {
+                gb.readBits(1);  // separate_colour_plane_flag
+            }
+            if (chroma_format_idc <= 3) {
+                chroma_format = chroma_format_table[chroma_format_idc];
+            }
+
+            bit_depth = gb.readUEG() + 8;  // bit_depth_luma_minus8
+            gb.readUEG();  // bit_depth_chroma_minus8
+            gb.readBits(1);  // qpprime_y_zero_transform_bypass_flag
+            if (gb.readBool()) {  // seq_scaling_matrix_present_flag
+                let scaling_list_count = (chroma_format_idc !== 3) ? 8 : 12;
+                for (let i = 0; i < scaling_list_count; i++) {
+                    if (gb.readBool()) {  // seq_scaling_list_present_flag
+                        if (i < 6) {
+                            SPSParser._skipScalingList(gb, 16);
+                        } else {
+                            SPSParser._skipScalingList(gb, 64);
+                        }
+                    }
+                }
+            }
+        }
+        gb.readUEG();  // log2_max_frame_num_minus4
+        let pic_order_cnt_type = gb.readUEG();
