39424fc748de430c50ffba35d3a22d863edb5f03
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-03-15 09:57:57 +0000

    Adding new results

--- src/io/fetch-stream-loader.js
+++ src/io/fetch-stream-loader.js
@@ -1,2 +1,84 @@
 
 /*
+ * Copyright (C) 2016 Bilibili. All Rights Reserved.
+ *
+ * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Log from '../utils/logger.js';
+import Browser from '../utils/browser.js';
+import {BaseLoader, LoaderStatus, LoaderErrors} from './loader.js';
+import {RuntimeException} from '../utils/exception.js';
+
+/* fetch + stream IO loader. Currently working on chrome 43+.
+ * fetch provides a better alternative http API to XMLHttpRequest
+ *
+ * fetch spec   https://fetch.spec.whatwg.org/
+ * stream spec  https://streams.spec.whatwg.org/
+ */
+class FetchStreamLoader extends BaseLoader {
+
+    static isSupported() {
+        try {
+            // fetch + stream is broken on Microsoft Edge. Disable before build 15048.
+            // see https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/8196907/
+            // Fixed in Jan 10, 2017. Build 15048+ removed from blacklist.
+            let isWorkWellEdge = Browser.msedge && Browser.version.minor >= 15048;
+            let browserNotBlacklisted = Browser.msedge ? isWorkWellEdge : true;
+            return (self.fetch && self.ReadableStream && browserNotBlacklisted);
+        } catch (e) {
+            return false;
+        }
+    }
+
+    constructor(seekHandler, config) {
+        super('fetch-stream-loader');
+        this.TAG = 'FetchStreamLoader';
+
+        this._seekHandler = seekHandler;
+        this._config = config;
+        this._needStash = true;
+
+        this._requestAbort = false;
+        this._contentLength = null;
+        this._receivedLength = 0;
+    }
+
+    destroy() {
+        if (this.isWorking()) {
+            this.abort();
+        }
+        super.destroy();
+    }
+
+    open(dataSource, range) {
+        this._dataSource = dataSource;
+        this._range = range;
+
+        let sourceURL = dataSource.url;
+        if (this._config.reuseRedirectedURL && dataSource.redirectedURL != undefined) {
+            sourceURL = dataSource.redirectedURL;
+        }
+
+        let seekConfig = this._seekHandler.getConfig(sourceURL, range);
+
+        let headers = new self.Headers();
+
+        if (typeof seekConfig.headers === 'object') {
+            let configHeaders = seekConfig.headers;
+            for (let key in configHeaders) {
+                if (configHeaders.hasOwnProperty(key)) {
+                    headers.append(key, configHeaders[key]);
+                }
