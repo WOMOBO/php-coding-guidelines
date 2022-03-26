63630aea1354a4d58c403aeac1eedc382fe8de0c
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-03-26 16:23:30 +0000

    Lint code

--- src/io/param-seek-handler.js
+++ src/io/param-seek-handler.js
@@ -18,3 +18,69 @@
  */
 
 class ParamSeekHandler {
+
+    constructor(paramStart, paramEnd) {
+        this._startName = paramStart;
+        this._endName = paramEnd;
+    }
+
+    getConfig(baseUrl, range) {
+        let url = baseUrl;
+
+        if (range.from !== 0 || range.to !== -1) {
+            let needAnd = true;
+            if (url.indexOf('?') === -1) {
+                url += '?';
+                needAnd = false;
+            }
+
+            if (needAnd) {
+                url += '&';
+            }
+
+            url += `${this._startName}=${range.from.toString()}`;
+
+            if (range.to !== -1) {
+                url += `&${this._endName}=${range.to.toString()}`;
+            }
+        }
+
+        return {
+            url: url,
+            headers: {}
+        };
+    }
+
+    removeURLParameters(seekedURL) {
+        let baseURL = seekedURL.split('?')[0];
+        let params = undefined;
+
+        let queryIndex = seekedURL.indexOf('?');
+        if (queryIndex !== -1) {
+            params = seekedURL.substring(queryIndex + 1);
+        }
+
+        let resultParams = '';
+
+        if (params != undefined && params.length > 0) {
+            let pairs = params.split('&');
+
+            for (let i = 0; i < pairs.length; i++) {
+                let pair = pairs[i].split('=');
+                let requireAnd = (i > 0);
+
+                if (pair[0] !== this._startName && pair[0] !== this._endName) {
+                    if (requireAnd) {
+                        resultParams += '&';
+                    }
+                    resultParams += pairs[i];
+                }
+            }
+        }
+
+        return (resultParams.length === 0) ? baseURL : baseURL + '?' + resultParams;
+    }
+
+}
+
+export default ParamSeekHandler;
