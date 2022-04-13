6ee216ae3ac69da2ad6892763a233f16e63fd17e
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-04-13 11:13:42 +0000

    new bg

--- src/io/speed-sampler.js
+++ src/io/speed-sampler.js
@@ -14,4 +14,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+// Utility class to calculate realtime network I/O speed
+class SpeedSampler {
+
+    
