7e44b37f6a16837ac28458c1f1306791f4db1584
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-05-19 15:36:40 +0000

    Add latest Ansible versions (2.5.12, 2.6.8, 2.6.9, 2.7.2, 2.7.3) And fix some warnings

--- src/io/xhr-msstream-loader.js
+++ src/io/xhr-msstream-loader.js
@@ -26,3 +26,89 @@ import {RuntimeException} from '../utils/exception.js';
  *
  * For IE11/Edge browser by microsoft which supports `xhr.responseType = 'ms-stream'`
  * Notice that ms-stream API sucks. The buffer is always expanding along with downloading.
+ *
+ * We need to abort the xhr if buffer size exceeded limit size (e.g. 16 MiB), then do reconnect.
+ * in order to release previous ArrayBuffer to avoid memory leak
+ *
+ * Otherwise, the ArrayBuffer will increase to a terrible size that equals final file size.
+ */
+class MSStreamLoader extends BaseLoader {
+
+    static isSupported() {
+        try {
+            if (typeof self.MSStream === 'undefined' || typeof self.MSStreamReader === 'undefined') {
+                return false;
+            }
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://example.com', true);
+            xhr.responseType = 'ms-stream';
+            return (xhr.responseType === 'ms-stream');
+        } catch (e) {
+            Log.w('MSStreamLoader', e.message);
+            return false;
+        }
+    }
+
+    constructor(seekHandler, config) {
+        super('xhr-msstream-loader');
+        this.TAG = 'MSStreamLoader';
+
+        this._seekHandler = seekHandler;
+        this._config = config;
+        this._needStash = true;
+
+        this._xhr = null;
+        this._reader = null;  // MSStreamReader
+
+        this._totalRange = null;
+        this._currentRange = null;
+
+        this._currentRequestURL = null;
+        this._currentRedirectedURL = null;
+
+        this._contentLength = null;
+        this._receivedLength = 0;
+
+        this._bufferLimit = 16 * 1024 * 1024;  // 16MB
+        this._lastTimeBufferSize = 0;
+        this._isReconnecting = false;
+    }
+
+    destroy() {
+        if (this.isWorking()) {
+            this.abort();
+        }
+        if (this._reader) {
+            this._reader.onprogress = null;
+            this._reader.onload = null;
+            this._reader.onerror = null;
+            this._reader = null;
+        }
+        if (this._xhr) {
+            this._xhr.onreadystatechange = null;
+            this._xhr = null;
+        }
+        super.destroy();
+    }
+
+    open(dataSource, range) {
+        this._internalOpen(dataSource, range, false);
+    }
+
+    _internalOpen(dataSource, range, isSubrange) {
+        this._dataSource = dataSource;
+
+        if (!isSubrange) {
+            this._totalRange = range;
+        } else {
+            this._currentRange = range;
+        }
+
+        let sourceURL = dataSource.url;
+        if (this._config.reuseRedirectedURL) {
+            if (this._currentRedirectedURL != undefined) {
+                sourceURL = this._currentRedirectedURL;
+            } else if (dataSource.redirectedURL != undefined) {
+                sourceURL = dataSource.redirectedURL;
+            }
