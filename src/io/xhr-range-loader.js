755141ae5b9e6b8cd75aba8f7ce2de29ed80254f
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-01 22:30:04 +0000

    4.2.1

--- src/io/xhr-range-loader.js
+++ src/io/xhr-range-loader.js
@@ -39,3 +39,82 @@ class RangeLoader extends BaseLoader {
 
     constructor(seekHandler, config) {
         super('xhr-range-loader');
+        this.TAG = 'RangeLoader';
+
+        this._seekHandler = seekHandler;
+        this._config = config;
+        this._needStash = false;
+
+        this._chunkSizeKBList = [
+            128, 256, 384, 512, 768, 1024, 1536, 2048, 3072, 4096, 5120, 6144, 7168, 8192
+        ];
+        this._currentChunkSizeKB = 384;
+        this._currentSpeedNormalized = 0;
+        this._zeroSpeedChunkCount = 0;
+
+        this._xhr = null;
+        this._speedSampler = new SpeedSampler();
+
+        this._requestAbort = false;
+        this._waitForTotalLength = false;
+        this._totalLengthReceived = false;
+
+        this._currentRequestURL = null;
+        this._currentRedirectedURL = null;
+        this._currentRequestRange = null;
+        this._totalLength = null;  // size of the entire file
+        this._contentLength = null;  // Content-Length of entire request range
+        this._receivedLength = 0;  // total received bytes
+        this._lastTimeLoaded = 0;  // received bytes of current request sub-range
+    }
+
+    destroy() {
+        if (this.isWorking()) {
+            this.abort();
+        }
+        if (this._xhr) {
+            this._xhr.onreadystatechange = null;
+            this._xhr.onprogress = null;
+            this._xhr.onload = null;
+            this._xhr.onerror = null;
+            this._xhr = null;
+        }
+        super.destroy();
+    }
+
+    get currentSpeed() {
+        return this._speedSampler.lastSecondKBps;
+    }
+
+    open(dataSource, range) {
+        this._dataSource = dataSource;
+        this._range = range;
+        this._status = LoaderStatus.kConnecting;
+
+        let useRefTotalLength = false;
+        if (this._dataSource.filesize != undefined && this._dataSource.filesize !== 0) {
+            useRefTotalLength = true;
+            this._totalLength = this._dataSource.filesize;
+        }
+
+        if (!this._totalLengthReceived && !useRefTotalLength) {
+            // We need total filesize
+            this._waitForTotalLength = true;
+            this._internalOpen(this._dataSource, {from: 0, to: -1});
+        } else {
+            // We have filesize, start loading
+            this._openSubRange();
+        }
+    }
+
+    _openSubRange() {
+        let chunkSize = this._currentChunkSizeKB * 1024;
+
+        let from = this._range.from + this._receivedLength;
+        let to = from + chunkSize;
+
+        if (this._contentLength != null) {
+            if (to - this._range.from >= this._contentLength) {
+                to = this._range.from + this._contentLength - 1;
+            }
+        }
