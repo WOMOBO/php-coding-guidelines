abddce14126ef1195fc550854937c873eb6f9a94
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-11 06:53:19 +0000

    VB

--- src/player/native-player.js
+++ src/player/native-player.js
@@ -7,3 +7,80 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import EventEmitter from 'events';
+import PlayerEvents from './player-events.js';
+import {createDefaultConfig} from '../config.js';
+import {InvalidArgumentException, IllegalStateException} from '../utils/exception.js';
+
+// Player wrapper for browser's native player (HTMLVideoElement) without MediaSource src. 
+class NativePlayer {
+
+    constructor(mediaDataSource, config) {
+        this.TAG = 'NativePlayer';
+        this._type = 'NativePlayer';
+        this._emitter = new EventEmitter();
+
+        this._config = createDefaultConfig();
+        if (typeof config === 'object') {
+            Object.assign(this._config, config);
+        }
+
+        if (mediaDataSource.type.toLowerCase() === 'flv') {
+            throw new InvalidArgumentException('NativePlayer does\'t support flv MediaDataSource input!');
+        }
+        if (mediaDataSource.hasOwnProperty('segments')) {
+            throw new InvalidArgumentException(`NativePlayer(${mediaDataSource.type}) doesn't support multipart playback!`);
+        }
+
+        this.e = {
+            onvLoadedMetadata: this._onvLoadedMetadata.bind(this)
+        };
+
+        this._pendingSeekTime = null;
+        this._statisticsReporter = null;
+
+        this._mediaDataSource = mediaDataSource;
+        this._mediaElement = null;
+    }
+
+    destroy() {
+        if (this._mediaElement) {
+            this.unload();
+            this.detachMediaElement();
+        }
+        this.e = null;
+        this._mediaDataSource = null;
+        this._emitter.removeAllListeners();
+        this._emitter = null;
+    }
+
+    on(event, listener) {
+        if (event === PlayerEvents.MEDIA_INFO) {
+            if (this._mediaElement != null && this._mediaElement.readyState !== 0) {  // HAVE_NOTHING
+                Promise.resolve().then(() => {
+                    this._emitter.emit(PlayerEvents.MEDIA_INFO, this.mediaInfo);
+                });
+            }
+        } else if (event === PlayerEvents.STATISTICS_INFO) {
+            if (this._mediaElement != null && this._mediaElement.readyState !== 0) {
+                Promise.resolve().then(() => {
+                    this._emitter.emit(PlayerEvents.STATISTICS_INFO, this.statisticsInfo);
+                });
+            }
+        }
+        this._emitter.addListener(event, listener);
+    }
+
+    off(event, listener) {
+        this._emitter.removeListener(event, listener);
+    }
