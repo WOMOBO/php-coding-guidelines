9c01d1ea3e3cb6ead3d07faa695a76858b9d5d1f
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-16 12:27:00 +0000

    tests/includes/PDO_DummyStatement.php

--- src/player/player-events.js
+++ src/player/player-events.js
@@ -3,4 +3,5 @@
  *
  * @author zheng qian <xqq@xqq.im>
  *
- * Licensed under the Apache L
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in co
