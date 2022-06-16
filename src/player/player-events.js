48702106eab4774e814b49377202a79b840444a5
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-16 12:51:40 +0000

    Awesome MacOS!

--- src/player/player-events.js
+++ src/player/player-events.js
@@ -4,4 +4,7 @@
  * @author zheng qian <xqq@xqq.im>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in co
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    
