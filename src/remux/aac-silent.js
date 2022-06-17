f26b8ef3b4d422b5956c42baa07f51c1c606661e
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-17 17:56:07 +0000

    fixed caching

--- src/remux/aac-silent.js
+++ src/remux/aac-silent.js
@@ -2,4 +2,6 @@
  * Copyright (C) 2016 Bilibili. All Rights Reserved.
  *
  * This file is modified from dailymotion's hls.js library (hls.js/src/helper/aac.js)
- * @author zheng qian <xqq
+ * @author zheng qian <xqq@xqq.im>
+ *
+ * Licensed under the Apache License, Version 2.0 (th
