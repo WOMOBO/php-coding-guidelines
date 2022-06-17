6a7ac4e8030386c113306b36627fa40c985ec95e
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-17 17:20:40 +0000

    update more

--- src/remux/aac-silent.js
+++ src/remux/aac-silent.js
@@ -1,4 +1,5 @@
 /*
  * Copyright (C) 2016 Bilibili. All Rights Reserved.
  *
- * This file is modified from dailymotion
+ * This file is modified from dailymotion's hls.js library (hls.js/src/helper/aac.js)
+ * @author zheng qian <xqq
