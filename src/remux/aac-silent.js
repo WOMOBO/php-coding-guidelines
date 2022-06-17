cbb56fdd21ce3257e5056fed8f941fb3424104d6
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-17 18:11:33 +0000

    Publish page-9 index

--- src/remux/aac-silent.js
+++ src/remux/aac-silent.js
@@ -4,4 +4,6 @@
  * This file is modified from dailymotion's hls.js library (hls.js/src/helper/aac.js)
  * @author zheng qian <xqq@xqq.im>
  *
- * Licensed under the Apache License, Version 2.0 (th
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ 
