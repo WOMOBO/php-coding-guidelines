c90ddcbd18c913a5e00399dfe8c69782fbbbc4a8
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-06-17 10:36:33 +0000

    skge: Add DMA mask quirk for Marvell 88E8001 on ASUS P5NSLI motherboard

--- src/player/player-events.js
+++ src/player/player-events.js
@@ -20,4 +20,6 @@ const PlayerEvents = {
     ERROR: 'error',
     LOADING_COMPLETE: 'loading_complete',
     RECOVERED_EARLY_EOF: 'recovered_early_eof',
-    MEDIA_INFO: 'media_
+    MEDIA_INFO: 'media_info',
+    METADATA_ARRIVED: 'metadata_arrived',
+    SCRIPTDATA_ARRIVED: 'scriptdata_arri
