0dab94d7b4565420891b70058c8877fc63ef517f
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2022-10-15 16:04:47 +0000

    Update godoc

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -691,4 +691,6 @@ class MP4Remuxer {
         mdatbox = new Uint8Array(mdatBytes);
         mdatbox[0] = (mdatBytes >>> 24) & 0xFF;
         mdatbox[1] = (mdatBytes >>> 16) & 0xFF;
-        mdatbox
+        mdatbox[2] = (mdatBytes >>> 8) & 0xFF;
+        mdatbox[3] = (mdatBytes) & 0xFF;
+        mdatbox.se
