b554840f94baf1a17a10f6f5992653afce26127b
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2023-01-11 09:48:17 +0000

    Updated year from all PHP Headers

--- src/utils/logging-control.js
+++ src/utils/logging-control.js
@@ -19,3 +19,74 @@
 
 import EventEmitter from 'events';
 import Log from './logger.js';
+
+class LoggingControl {
+
+    static get forceGlobalTag() {
+        return Log.FORCE_GLOBAL_TAG;
+    }
+
+    static set forceGlobalTag(enable) {
+        Log.FORCE_GLOBAL_TAG = enable;
+        LoggingControl._notifyChange();
+    }
+
+    static get globalTag() {
+        return Log.GLOBAL_TAG;
+    }
+
+    static set globalTag(tag) {
+        Log.GLOBAL_TAG = tag;
+        LoggingControl._notifyChange();
+    }
+
+    static get enableAll() {
+        return Log.ENABLE_VERBOSE
+            && Log.ENABLE_DEBUG
+            && Log.ENABLE_INFO
+            && Log.ENABLE_WARN
+            && Log.ENABLE_ERROR;
+    }
+
+    static set enableAll(enable) {
+        Log.ENABLE_VERBOSE = enable;
+        Log.ENABLE_DEBUG = enable;
+        Log.ENABLE_INFO = enable;
+        Log.ENABLE_WARN = enable;
+        Log.ENABLE_ERROR = enable;
+        LoggingControl._notifyChange();
+    }
+
+    static get enableDebug() {
+        return Log.ENABLE_DEBUG;
+    }
+
+    static set enableDebug(enable) {
+        Log.ENABLE_DEBUG = enable;
+        LoggingControl._notifyChange();
+    }
+
+    static get enableVerbose() {
+        return Log.ENABLE_VERBOSE;
+    }
+
+    static set enableVerbose(enable) {
+        Log.ENABLE_VERBOSE = enable;
+        LoggingControl._notifyChange();
+    }
+
+    static get enableInfo() {
+        return Log.ENABLE_INFO;
+    }
+
+    static set enableInfo(enable) {
+        Log.ENABLE_INFO = enable;
+        LoggingControl._notifyChange();
+    }
+
+    static get enableWarn() {
+        return Log.ENABLE_WARN;
+    }
+
+    static set enableWarn(enable) {
+        Log.ENABLE_WARN = enable;
