4a166f113e4f8ffd50f6c992d20c05e5e559ea5f
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2023-02-07 14:58:29 +0000

    crypto: msm: change dev_err messages to dev_warn for reaper worker

--- types/test-flv.ts
+++ types/test-flv.ts
@@ -1,4 +1,7 @@
 import flvjs from '../';
 
 type LoaderStatusAlias = flvjs.LoaderStatus;
-type LoaderErrorsAlias = flvjs.Load
+type LoaderErrorsAlias = flvjs.LoaderErrors;
+
+interface MediaDataSourceExt extends flvjs.MediaDataSource {
+    example
