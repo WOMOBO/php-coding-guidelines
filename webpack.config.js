156f36382ab178f5b9b671d69dc4a8c4b57be3a5
WOMOBO <104519614+WOMOBO@users.noreply.github.com>
2023-02-10 13:18:20 +0000

    0.1.9

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,6 @@ const TerserPlugin = require('terser-webpack-plugin');
 let config = {
     entry: './src/index.js',
     output: {
-        f
+        filename: 'flv.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'fl
